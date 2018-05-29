6c630e124b2f3caac32ea5248a71fba907596597
jules <ntjules@gmail.com>
2018-05-29 23:39:44 +0000

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+   get    '/'             ,to: 'sessions#new' ,as: 'home'
   resources :sessions, only: [:new, :create, :destroy]
   resources :prices, only: [:new, :create]
   resources :users
